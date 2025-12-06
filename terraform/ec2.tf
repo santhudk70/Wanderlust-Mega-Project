81279a4c868a95df53b612ff9c9164c6dd72f781
santhudk70 <133210540+santhudk70@users.noreply.github.com>
2025-12-06 12:15:10 +0530

    Update ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_key_pair" "deployer" {
-  key_name   = "terra-automate-key"
+  key_name   = "terra-automate-key-new"
   public_key = file("C:\\Users\\Administrator\\mega-project\\Wanderlust-Mega-Project\\terraform\\terra-key.pub")
 }
 
